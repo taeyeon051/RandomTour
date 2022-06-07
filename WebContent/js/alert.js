753e0006e7c30b30de8e5ca495c379311d377680
taeyeon051 <taeyeon051@naver.com>
2022-06-07 12:07:38 +0900

    alert창 수정

--- WebContent/js/alert.js
+++ WebContent/js/alert.js
@@ -2,10 +2,10 @@ window.Alert = (state, text) => {
     if (document.querySelector(".alert")) return;
     const div = document.createElement("div");
     div.innerHTML =
-        `<div class="alert alert-${state} alert-dismissible fade show" role="alert">
-		    ${text}
-		    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-	    </div>`;
+    	`<div class="alert alert-${state} alert-dismissible fade show" role="alert">
+    		${text}
+    		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    	</div>`;
     document.querySelector("body").appendChild(div);
     setTimeout(() => { div.remove(); }, 3000);
 }
