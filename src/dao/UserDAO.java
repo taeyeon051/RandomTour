3e7af944017b8ae432f78251556531de8e316a1e
taeyeon051 <taeyeon051@naver.com>
2022-06-12 20:41:33 +0900

    경로 수정

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import common.JdbcUtil;
+import util.JdbcUtil;
 import vo.UserVO;
 
 public class UserDAO {
