d487db2f406ef431cd42971caa0084f9a2db4c2e
taeyeon051 <taeyeon051@naver.com>
2022-07-02 22:44:20 +0900

    마이페이지 생성

---
+++ WebContent/views/myPage.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="./components/Header.jsp" %>
+
+<jsp:include page="./components/Background.jsp" />
+
+<section>
+	<jsp:include page="./components/Logo.jsp" />
+	
+	<div>
+		
+	</div>
+</section>
+
+<script src="<%=path%>/js/user/myPage.js"></script>
+
+<%@ include file="./components/Footer.jsp" %>
---
+++ src/controller/user/MyPageController.java
@@ -0,0 +1,25 @@
+package controller.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.Controller;
+import controller.MyView;
+import vo.UserVO;
+
+public class MyPageController implements Controller {
+	@Override
+	public MyView process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		UserVO user = (UserVO) session.getAttribute("randomtour-user");
+		if (user == null) return new MyView("/user/login", true);
+		
+		return new MyView("/views/myPage.jsp");
+	}
+
+}
